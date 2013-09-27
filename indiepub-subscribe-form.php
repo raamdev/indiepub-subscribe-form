636bae190ba172e4ea679eca52946629ca06a89d
Raam Dev <raam@raamdev.com>
2013-09-27 18:16:39 -0400

    Add .gitignore and indiepub-subscribe-form.php

---
+++ indiepub-subscribe-form.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+Plugin Name: IndiePub Subscribe Form
+Plugin URI: http://independentpublisher.me/plugins/indiepub-subscribe-form/
+Description: Plugin for displaying subscription options.
+Author: Raam Dev
+Version: 1.0 
+Author URI: http://raamdev.com/
+*/
